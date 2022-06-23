d6d5ce7e0c07936bfd32403b0dda2bc5b7b9ffd0
Faith Umunnakwe <faithuchinonso@gmail.com>
2022-06-23 11:29:25 +0100

    new style file

--- src/component/View/Intro.js
+++ src/component/View/Intro.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import "./style.css";
+import "./view.css";
 
 const Intro = props => {
   return (
--- src/component/View/Nav.js
+++ src/component/View/Nav.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 // import "./style.css";
+import "./view.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
